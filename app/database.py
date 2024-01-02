2b14b1952c54353b671c6832477d95b110ff475c
Kuno Claes <kunoclaes@Kunos-MacBook-Pro.local>
2024-01-02 23:20:18 +0100

    Finishing touches

--- app/database.py
+++ app/database.py
@@ -3,15 +3,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-print("PRINT")
+# Fixed some errors cuzz okteto being hard
 if not os.path.exists('./sqlitedb'):
     os.makedirs('./sqlitedb')
 
+# Make connection to DB
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitedb/sqlitedata.db"
 
+
+
+# Create db engine and session
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
+# Define the base we use for commuinication with the db
 Base = sqlalchemy.orm.declarative_base()
