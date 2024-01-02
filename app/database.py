ec6c5720579908b30dae09af924ff82c35c2cf4d
Kuno Claes(D) <kuno@icodes.dev>
2024-01-02 13:07:18 +0100

    Update

--- app/database.py
+++ app/database.py
@@ -1,25 +1,18 @@
-from sqlalchemy import create_engine, Column, Integer, String
+import sqlalchemy
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import os
 
+print("PRINT")
+if not os.path.exists('./sqlitedb'):
+    os.makedirs('./sqlitedb')
 
-# Creëer een engine om verbinding te maken met de SQLite-database (in dit geval)
-# SQLALCHEMY_DATABASE_URL = "sqlite:///db/books.db"
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitedb/sqlitedata.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+Base = sqlalchemy.orm.declarative_base()
