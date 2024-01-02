eb0454e8996b9c8160fc9834ac2d7ae8bdb0c2f7
Kuno Claes(D) <kuno@icodes.dev>
2024-01-02 13:18:09 +0100

    Update

--- app/database.py
+++ app/database.py
@@ -8,7 +8,6 @@ if not os.path.exists('./sqlitedb'):
     os.makedirs('./sqlitedb')
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitedb/sqlitedata.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
