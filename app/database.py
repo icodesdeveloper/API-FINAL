b9c69b7ad07eb623b9e81cf56be866731133c662
Kuno Claes(D) <kuno@icodes.dev>
2024-01-02 12:57:04 +0100

    Update

--- app/database.py
+++ app/database.py
@@ -1,18 +1,20 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import pymysql
+
 
 # Creëer een engine om verbinding te maken met de SQLite-database (in dit geval)
 # SQLALCHEMY_DATABASE_URL = "sqlite:///db/books.db"
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitedb/sqlitedata.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# Maak een sessiemaker voor het beheren van de sessies met de database
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Maak een Base-klasse voor declaratieve class mapping
 Base = declarative_base()
+
 
 # Dependency
 def get_db():
